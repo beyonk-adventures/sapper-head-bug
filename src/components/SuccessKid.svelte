b62cdf014161bc31788d27a81dead8229c9f7514
Antony Jones <antony@beyonk.com>
2020-06-11 17:54:57 +0100

    initial commit

---
+++ src/components/SuccessKid.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <title>I preferrred Borat</title>
+</svelte:head>
+
+<figure>
+	<img alt='Success Kid' src='successkid.jpg'>
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<style>
+	figure {
+		margin: 0 0 1em 0;
+    text-align: center;
+		margin: 0 auto;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+</style>
